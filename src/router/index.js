aec101cc761a18bdd9c4599a17330337a102f9b4
JanZon00 <zonjasiek@wp.pl>
2024-03-16 12:49:35 +0100

    removed authorization from all pages except admin page

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ const routes = [
     path: '/',
     name: 'home',
     component: () => import('../views/HomeView.vue'),
-    meta: {requiresAuth: true},
   },
   {
     path: '/register',
@@ -22,19 +21,16 @@ const routes = [
     path: '/page1',
     name: 'page1',
     component: () => import('../views/Page1View.vue'),
-    meta: {requiresAuth: true},
   },
   {
     path: '/page2',
     name: 'page2',
     component: () => import('../views/Page2View.vue'),
-    meta: {requiresAuth: true},
   },
   {
     path: '/page3',
     name: 'page3',
     component: () => import('../views/Page3View.vue'),
-    meta: {requiresAuth: true},
   },
   {
     path: '/settings',
