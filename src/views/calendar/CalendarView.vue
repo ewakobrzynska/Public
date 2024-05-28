6c1a91d7e17dd1aea10e030072da50392fa10a02
TypicalMonkey <guineapigofallah@gmail.com>
2024-05-28 20:13:40 +0200

    addtodatabsae

--- src/views/calendar/CalendarView.vue
+++ src/views/calendar/CalendarView.vue
@@ -1,217 +1,205 @@
-<script>
-
-import FullCalendar from '@fullcalendar/vue3'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import DialogModal from './DialogModal.vue'
-import { ref } from 'vue'
-import { getEvents } from '../../../data/calendarEvents'
-
-
-export default {
-    components: {
-        FullCalendar,
-        DialogModal
-    },
-
-    data() {
-        return {
-            calendarOptions: {
-                plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin ],
-                headerToolbar: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                },
-                buttonText: {
-                    today: 'Dzisiaj',
-                    month: 'Miesiąc',
-                    day: 'Dzień',
-                    week: 'Tydzień',
-                },
-                locale: 'pl',
-                initialView: 'timeGridWeek',
-                weekends: false,
-                selectable: true,
-                nowIndicator: true,
-                slotDuration: "00:15:00",
-                slotLabelInterval: "00:15:00",
-                selectMirror: true,
-                dateClick: this.handleDateClick,
-                select: this.handleDateSelect,
-                eventsSet: this.handleEvents,
-                allDaySlot: false,
-                allDayText: "",
-                expandRows: true,
-                eventOverlap: false,
-                selectOverlap: false,
-            },
-            currentEvents: [],
-            parentState: ref(false),
-            formData: {},
-            tmpArg: {}
-
-        }
-    },
-
-    methods: {
-        handleDateClick: function(arg) {
-            if (arg.view['type'] == 'dayGridMonth') {
-                let calendarApi = arg.view.calendar
-                calendarApi.changeView('timeGridDay', arg.dateStr);
-            }
-        },
-
-        createEventId() {
-            return "id" + Math.random().toString(16).slice(2)
-        },
-
-        handleDateSelect(arg) {
-            if (arg.view['type'] == 'dayGridMonth') {
-                return
-            }
-            this.updateDialogState()
-            this.tmpArg = arg
-
-        },
-
-        performAddEvent(arg) {
-            const calendarApi = this.$refs.calendarRef.getApi();
-
-            calendarApi.unselect() 
-
-            const title = `${this.formData.course} - ${this.formData.firstName} ${this.formData.lastName}`;
-
-            calendarApi.addEvent({
-                id: this.createEventId(),
-                title: title,
-                start: arg.startStr,
-                end: arg.endStr,
-                allDay: arg.allDay
-            })
-
-        },
-
-        handleEvents(events) {
-            this.currentEvents = events
-        },
-
-        updateDialogState() {
-            this.parentState = !this.parentState;
-            console.log(this.parentState)
-        },
-
-        // handleClosingDialog(data) {
-        //     this.parentState = data
-        //     console.log(this.parentState)
-        // },
-
-        handleSubmition(args) {
-            console.log(args)
-            this.formData = args
-            this.updateDialogState()
-            this.performAddEvent(this.tmpArg)
-        },
-
-        loadEvents(events) {
-            const numberOfEvents = events.length
-
-            // console.log(events[0])
-            
-            const calendarApi = this.$refs.calendarRef.getApi();
-            // let calendarApi = events[0].view.calendar
-
-            for (let i = 0; i < numberOfEvents; i++) {
-                const title = `${events[i].title} - ${events[i].person}`;
-                calendarApi.addEvent({
-                    id: this.createEventId(),
-                    title: title,
-                    start: events[i].startStr,
-                    end: events[i].endStr,
-                    allDay: events[i].allDay
-                });
-            }
-        }
-    },
-
-    async mounted() {
-        try {
-            const roomNumber = this.$route.params.roomNumber;
-
-            const events = getEvents(roomNumber);
-            
-            if (events instanceof Promise) {
-                const resolvedEvents = await events;
-                this.loadEvents(resolvedEvents)
-                console.log("Todo", resolvedEvents);
-            } else {
-                console.log("Todo", events);
-            }
-        } catch (error) {
-            console.error("Error fetching events:", error);
-        }
-    },
-}
-</script>
-
-
 <template>
     <div class="demo-app">
-        <header>
-            <h2>Plan zajęć odbywających się w sali {{ $route.params.roomNumber }}</h2>
-            <h3>III Kampus, budynek Wydziału Fizyki, Astronomii i Informatyki Stosowanej (Loj11)</h3>
-        </header>
-
-        <div>
-            <FullCalendar :options="calendarOptions" ref="calendarRef"/>
-        </div>
-
-        <!-- <button @click="updateDialogState">Update State</button> -->
-        <DialogModal :childProp="parentState" v-on:changeDialogValue="updateDialogState" v-on:form-submitted="handleSubmition"/>
-        
+      <header>
+        <h2>Plan zajęć odbywających się w sali {{ $route.params.roomNumber }}</h2>
+        <h3>III Kampus, budynek Wydziału Fizyki, Astronomii i Informatyki Stosowanej (Loj11)</h3>
+      </header>
+      <div>
+        <FullCalendar :options="calendarOptions" ref="calendarRef"/>
+      </div>
+      <DialogModal :childProp="parentState" v-on:changeDialogValue="updateDialogState" v-on:form-submitted="handleSubmition"/>
     </div>
-  
   </template>
-
-<style lang='css'>
-
-    header {
-        padding-bottom: 4rem;
-        padding-top: 3rem;
-        text-align: left;
-        max-width: 1100px;
-        margin: 0 auto;
-    }
-
-    ul {
-        margin: 0;
-        padding: 0 0 0 1.5em;
-    }
-
-    li {
-        margin: 1.5em 0;
-        padding: 0;
-    }
-
-    b {
-        margin-right: 3px;
-    }
-
-    .demo-app {
-        min-height: 100%;
-        font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-    /* font-size: 14px; */
-    }
-
-    .demo-app-main {
-        flex-grow: 1;
-        padding: 3em;
-    }
-
-    .fc { /* the calendar root */
-        max-width: 1100px;
-        margin: 0 auto;
-    }
-
-</style>
+  
+  <script>
+  import FullCalendar from '@fullcalendar/vue3'
+  import dayGridPlugin from '@fullcalendar/daygrid'
+  import timeGridPlugin from '@fullcalendar/timegrid'
+  import interactionPlugin from '@fullcalendar/interaction'
+  import DialogModal from './DialogModal.vue'
+  import { ref } from 'vue'
+  import { getDatabase, ref as dbRef, push } from "firebase/database";
+  import emailjs from 'emailjs-com';
+  import { getEvents } from '../../../data/calendarEvents'
+  
+  export default {
+      components: {
+          FullCalendar,
+          DialogModal
+      },
+      data() {
+          return {
+              calendarOptions: {
+                  plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin ],
+                  headerToolbar: {
+                      left: 'prev,next today',
+                      center: 'title',
+                      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                  },
+                  buttonText: {
+                      today: 'Dzisiaj',
+                      month: 'Miesiąc',
+                      day: 'Dzień',
+                      week: 'Tydzień',
+                  },
+                  locale: 'pl',
+                  initialView: 'timeGridWeek',
+                  weekends: false,
+                  selectable: true,
+                  nowIndicator: true,
+                  slotDuration: "00:15:00",
+                  slotLabelInterval: "00:15:00",
+                  selectMirror: true,
+                  dateClick: this.handleDateClick,
+                  select: this.handleDateSelect,
+                  eventsSet: this.handleEvents,
+                  allDaySlot: false,
+                  allDayText: "",
+                  expandRows: true,
+                  eventOverlap: false,
+                  selectOverlap: false,
+              },
+              currentEvents: [],
+              parentState: ref(false),
+              formData: {},
+              tmpArg: {}
+          }
+      },
+      methods: {
+          handleDateClick: function(arg) {
+              if (arg.view['type'] == 'dayGridMonth') {
+                  let calendarApi = arg.view.calendar
+                  calendarApi.changeView('timeGridDay', arg.dateStr);
+              }
+          },
+          createEventId() {
+              return "id" + Math.random().toString(16).slice(2)
+          },
+          handleDateSelect(arg) {
+              if (arg.view['type'] == 'dayGridMonth') {
+                  return
+              }
+              this.updateDialogState()
+              this.tmpArg = arg
+          },
+          async performAddEvent(arg) {
+              const calendarApi = this.$refs.calendarRef.getApi();
+              const roomNumber = this.$route.params.roomNumber;
+              calendarApi.unselect()
+              const title = `${this.formData.course} - ${this.formData.firstName} ${this.formData.lastName}`;
+              calendarApi.addEvent({
+                  id: this.createEventId(),
+                  title: title,
+                  start: arg.startStr,
+                  end: arg.endStr,
+                  allDay: arg.allDay
+              })
+              try {
+                  const formData = {
+                      date: arg.startStr.split('T')[0],
+                      finish: arg.endStr.split('T')[1].substring(0, 5),
+                      name: this.formData.course,
+                      person: `${this.formData.firstName} ${this.formData.lastName}`,
+                      roomNumber: roomNumber,
+                      start: arg.startStr.split('T')[1].substring(0, 5),
+                      status: 'PENDING'
+                  };
+                  const db = getDatabase();
+                  const scheduleRef = dbRef(db, 'schedule');
+                  await push(scheduleRef, formData);
+  
+                  const timestamp = new Date();
+                  const formattedDate = timestamp.toLocaleString('pl-PL', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' });
+                  const emailParams = {
+                      firstName: this.formData.firstName,
+                      lastName: this.formData.lastName,
+                      course: this.formData.course,
+                      timestamp: formattedDate
+                  };
+                  emailjs.send('service_9b8vyny', 'template_c6bm709', emailParams, '8GjRBTArYojMBsJkC')
+                      .then((response) => {
+                          console.log('E-mail wysłany pomyślnie', response.status, response.text);
+                      })
+                      .catch((error) => {
+                          console.error('Błąd przy wysyłaniu e-maila:', error);
+                      });
+              } catch (error) {
+                  console.error("Błąd podczas dodawania rezerwacji do bazy danych:", error);
+              }
+          },
+          handleEvents(events) {
+              this.currentEvents = events
+          },
+          updateDialogState() {
+              this.parentState = !this.parentState;
+          },
+          handleSubmition(args) {
+              this.formData = args;
+              this.updateDialogState();
+              this.performAddEvent(this.tmpArg);
+          },
+          loadEvents(events) {
+              const numberOfEvents = events.length
+              const calendarApi = this.$refs.calendarRef.getApi();
+              for (let i = 0; i < numberOfEvents; i++) {
+                  const title = `${events[i].title} - ${events[i].person}`;
+                  calendarApi.addEvent({
+                      id: this.createEventId(),
+                      title: title,
+                      start: events[i].startStr,
+                      end: events[i].endStr,
+                      allDay: events[i].allDay
+                  });
+              }
+          }
+      },
+      async mounted() {
+          try {
+              const roomNumber = this.$route.params.roomNumber;
+              const events = getEvents(roomNumber);
+              if (events instanceof Promise) {
+                  const resolvedEvents = await events;
+                  this.loadEvents(resolvedEvents)
+              } else {
+                  this.loadEvents(events)
+              }
+          } catch (error) {
+              console.error("Error fetching events:", error);
+          }
+      },
+  }
+  </script>
+  
+  <style lang='css'>
+  header {
+      padding-bottom: 4rem;
+      padding-top: 3rem;
+      text-align: left;
+      max-width: 1100px;
+      margin: 0 auto;
+  }
+  ul {
+      margin: 0;
+      padding: 0 0 0 1.5em;
+  }
+  li {
+      margin: 1.5em 0;
+      padding: 0;
+  }
+  b {
+      margin-right: 3px;
+  }
+  .demo-app {
+      min-height: 100%;
+      font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  }
+  .demo-app-main {
+      flex-grow: 1;
+      padding: 3em;
+  }
+  .fc {
+      max-width: 1100px;
+      margin: 0 auto;
+  }
+  </style>
+  
--- src/views/calendar/DialogModal.vue
+++ src/views/calendar/DialogModal.vue
@@ -4,34 +4,22 @@
       <v-card prepend-icon="mdi-account" title="Dokończ rezerwację">
         <v-card-text>
           <v-row dense>
-
             <v-col cols="12" md="6" sm="6">
               <v-text-field label="Imię*" v-model="firstName" required></v-text-field>
             </v-col>
-
             <v-col cols="12" md="6" sm="6">
               <v-text-field label="Nazwisko*" v-model="lastName" required></v-text-field>
             </v-col>
-
-            <!-- <v-col cols="12" md="4" sm="6">
-              <v-text-field label="Email*" v-model="email" required></v-text-field>
-            </v-col> -->
-
             <v-col cols="12" sm="12">
               <v-text-field hint="rezerwacja będzie nosić nazwę podaną w tym polu" label="Prowadzony kurs*" v-model="course" required></v-text-field>
             </v-col>
           </v-row>
-
           <small class="text-caption text-medium-emphasis">*pole wymagane</small>
         </v-card-text>
-
         <v-divider></v-divider>
-
         <v-card-actions>
           <v-spacer></v-spacer>
-
           <v-btn text="Zamknij" variant="plain" @click="$emit('changeDialogValue', false)"></v-btn>
-
           <v-btn color="primary" text="Gotowe" variant="tonal" @click="submitForm"></v-btn>
         </v-card-actions>
       </v-card>
@@ -41,12 +29,6 @@
 
 <script setup>
 import { ref, defineProps, watch, defineEmits } from 'vue';
-import { getDatabase, ref as dbRef, push } from "firebase/database";
-import emailjs from 'emailjs-com';
-
-const apiKey  = '8GjRBTArYojMBsJkC';
-const serviceID = 'service_9b8vyny';
-const templateID = 'template_c6bm709'; 
 
 const emit = defineEmits(['formSubmitted']);
 
@@ -63,65 +45,20 @@ watch(() => props.childProp, (newValue) => {
 const firstName = ref('');
 const lastName = ref('');
 const course = ref('');
-//const roomNumber = ref('');
-//const date = ref('');
-//const start = ref('');
-//const finish = ref('');
 
-const submitForm = async () => {
+const submitForm = () => {
   if (!firstName.value || !lastName.value || !course.value) {
     alert("Wypełnij wszystkie pola.");
     return; 
   }
 
-  console.log('Submitting form...');
-  console.log('First Name:', firstName.value);
-  console.log('Last Name:', lastName.value);
-  console.log('Course:', course.value);
-
   const formData = {
-    //date: date.value,
-    //finish: finish.value,
-    name: course.value,
-    person: firstName.value + " " + lastName.value,
-    //roomNumber: roomNumber.value,
-    //start: start.value,
-    status: 'IMPORTED'
-    //timestamp: new Date().toISOString()
+    firstName: firstName.value,
+    lastName: lastName.value,
+    course: course.value,
   };
 
   emit('formSubmitted', formData);
-
-  try {
-   
-    const db = getDatabase();
-    const scheduleRef = dbRef(db, 'schedule');
-    await push(scheduleRef, formData);
-    console.log("Rezerwacja dodana do tabeli schedule.");
-
-    const timestamp = new Date();
-    const formattedDate = timestamp.toLocaleString('pl-PL', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' });
-    const emailParams = {
-      firstName: firstName.value,
-      lastName: lastName.value,
-      course: course.value,
-      timestamp: formattedDate 
-    };
-
-    emailjs.send(serviceID, templateID, emailParams, apiKey )
-      .then((response) => {
-        console.log('E-mail wysłany pomyślnie', response.status, response.text);
-      })
-      .catch((error) => {
-        console.error('Błąd przy wysyłaniu e-maila:', error);
-      });
-
-  } catch (error) {
-    console.error("Błąd podczas dodawania rezerwacji do bazy danych:", error);
-  }
-
   dialog.value = false;
 };
 </script>
-
-
